937b1d1001b1bc73b3acb113c021df30f48afa14
Gurkirat Singh Dhillon <navilavi.kol123@gmail.com>
2024-04-29 20:10:26 +0530

    Added navbar

--- Evs_project/evs/views.py
+++ Evs_project/evs/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
  # Create your views here.
 
 def index(request):
-     return HttpResponse("Hello, world!")
+     return render(request, "evs/index.html")
 
 # Create your views here.
