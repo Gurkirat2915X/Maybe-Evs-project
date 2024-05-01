b47f4ec4f5dec0ad35017f3f0b62223bd2c0f2d9
Gurkirat Singh Dhillon <navilavi.kol123@gmail.com>
2024-05-01 21:27:32 +0530

    Added causes and some django work (GSD)

--- Evs_project/evs/urls.py
+++ Evs_project/evs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.index, name="index")
+     path("Home", views.index, name="index"),
+     path("Causes",views.Causes,name="Causes")
  ]
--- Evs_project/evs/views.py
+++ Evs_project/evs/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+nav_names = ["Home","Causes","Solutions","NGOs","Forms"]
+logo_link = "https://img.freepik.com/free-vector/sea-animal-cartoon-sticker-with-cute-fish_1308-75896.jpg?w=1380&t=st=1714400607~exp=1714401207~hmac=6f3a5b850685ab51e8bee2ac36ea2fad282bcd1b75d3b034fcb76846433b71a7"
+img_links = ["ABS","123"]
+causes = ["1","2","3","4"]
+link_causes = []
+
+for i in causes:
+     link_causes.append("#"+str(i))
  # Create your views here.
 
 def index(request):
-     return render(request, "evs/index.html")
+     return render(request, "evs/index.html",{"name":
+     "Home","nav_names":nav_names,"logo":logo_link,"images":img_links,
+     "img_links":img_links})
+def Causes(request):
+     return render(request,"evs/Causes.html",
+     {"name": nav_names[1],"nav_names":nav_names,"logo":logo_link
+     ,"causes":causes,"link_causes":link_causes})
 
 # Create your views here.
